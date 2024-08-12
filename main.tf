d2b934d1accdcfc570c91d93df672d74d6470179
Atem1972 <barronvalery@gmail.com>
2024-08-12 22:24:48 +0200

    import from cloute

--- main.tf
+++ main.tf
@@ -3,13 +3,21 @@ provider "aws" {
   
 }
 
+
+
+   # this import block is to ask terraform to go and get my resources from my instance that was created manually
 import {
   to = aws_instance.web
-  id = "i-054d428c6ea152799"
+  id = "i-03abf3bead3915984" #this instance i copy it from aws my instance  and past here
 }
+# first run the above code using terraform plan next run terraform plan terraform plan -generate-config-out=ec2.tf
+# it will pull the bellow arguement
+# this is the best way to modify instace we created manually in ec2 and pull it to terraform for modification
 
+  
+# this bellow is not important because the instance has been terminated. # to get this back run terraform plan -generate-config-out=ec2.tf
   # terraform will real generate the state file
- /*resource "aws_instance" "web" {
+ resource "aws_instance" "web" {
   ami                                  = "ami-03972092c42e8c0ca"
   associate_public_ip_address          = true
   availability_zone                    = "us-east-1b" 
@@ -17,12 +25,14 @@ import {
   ipv6_address_count                   = 0
   key_name                             = "valery"
   monitoring                           = false
-  security_groups                      = ["launch-wizard-3"]
+  security_groups                      = ["launch-wizard-4"]
   subnet_id                            = "subnet-07dceaf81154c4874"
   tags = {
     Name = "data-source"
   }
-  
+}
+  # after arranging the resource  run terraform apply--auto-approve, it will go to aws and import my resource. run ls u will see ur state file
+  # we can modify it ex, instead of t2.medium i change to t3.micro. run terraform plan i will go to aws and modify ur change
  
-  }
-  */
+  
+  
